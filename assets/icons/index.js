0a8363824b65d561e9d5e90009eda433e8143e16
Barış <dumanist00@gmail.com>
2020-05-07 08:03:18 +0300

    Icon files

---
+++ assets/icons/index.js
@@ -0,0 +1,8 @@
+export const icons = {
+    SJF :  require('./SJF.png'),
+    SRTF :  require('./SRTF.png'), 
+    FCFS :  require('./FCFS.png'),
+    RR :  require('./RR.png'),
+    PS :  require('./PS.png'),
+    MLQ :  require('./MLQ.png')
+}
