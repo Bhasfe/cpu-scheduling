f0ce894194638f010a738489be8057b6dc88a1ee
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-05-08 01:39:20 +0300

    function names added

--- data/algorithms-data.js
+++ data/algorithms-data.js
@@ -2,9 +2,9 @@ import algorithm from '../models/algorithm';
 
 export const ALGORITHMS = [
     new algorithm('0a','SJF',"SJF",false,"SJF"),
-    new algorithm('1a','SRTF',"SRTF",true,()=>{console.log('SRTF function!');}),
+    new algorithm('1a','SRTF',"SRTF",true,"SRTF"),
     new algorithm('2a','FCFS',"FCFS",false,"FCFS"),
-    new algorithm('3a','Round Robin',"RR",()=>{console.log('RR function !');}),
-    new algorithm('4a','Priority Scheduling',"PS",()=>{console.log('Priority Scheduling function !');}),
-    new algorithm('5a','Multilevel Queue Scheduling',"MLQ",()=>{console.log('Multilevel Queue Scheduling Function !');}),
+    new algorithm('3a','Round Robin',"RR",false,"RR"),
+    new algorithm('4a','Priority Scheduling',"PS",true,"PS"),
+    new algorithm('5a','Multilevel Queue Scheduling',"MLQ",true,"MLQ"),
 ]
