2fc58eb486dec27218de6ab898db106e92a96d24
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-05-08 21:19:56 +0300

    some colors added

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,6 +1,8 @@
 export default {
     primaryColor : '#fff',
     backgroundColor : '#292929',
+    borderColorSimulator : '#7D7D7D',
+    bgBubble : '#E58769',
     accentColor : '',
     titleColor : '',
 }
