5641780724e291493f8f1cdd4448adbb37d2d450
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-05-08 21:20:41 +0300

    algorithms updated

--- data/algorithms-data.js
+++ data/algorithms-data.js
@@ -5,6 +5,4 @@ export const ALGORITHMS = [
     new algorithm('1a','SRTF',"SRTF",true,"SRTF"),
     new algorithm('2a','FCFS',"FCFS",false,"FCFS"),
     new algorithm('3a','Round Robin',"RR",false,"RR"),
-    new algorithm('4a','Priority Scheduling',"PS",true,"PS"),
-    new algorithm('5a','Multilevel Queue Scheduling',"MLQ",true,"MLQ"),
 ]
