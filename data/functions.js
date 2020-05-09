b033c45d7cd9c26a0f80800ca0bcf03e7b28f9fb
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-05-09 23:46:18 +0300

    comments on functions

--- data/functions.js
+++ data/functions.js
@@ -1,5 +1,8 @@
 import process from '../models/process';
 
+
+// Shortest Job First function
+
 export const SJF = (processesCopy) => {
 
     var currentTime = 0;
@@ -7,11 +10,14 @@ export const SJF = (processesCopy) => {
     var finalExecution = [];
     var averageWait = 0;
 
+    // flag is used to check whether all processes in processesCopy is finished or not
     var flag = processesCopy.length;
 
     do {
+        // filtered stores processes that are available to run at current time
         var filtered = processesCopy.filter(p => p.arrivingTime <= currentTime && (!finalExecution.includes(p) || p.name === 'idle'));
         if (filtered.length != 0) {
+            // min stores the CPU burst time of process that has minimum CPU burst time among other processes that are available to run
             var min = filtered[0].cpuBurstTime1;
             var index = 0;
             for (let i = 0; i < filtered.length; i++) {
@@ -30,26 +36,27 @@ export const SJF = (processesCopy) => {
             filtered[index].tat = filtered[index].finish - filtered[index].arrivingTime;
             averageWait += filtered[index].wat;
 
-
+            // current time is incremented as CPU burst time of the process
             currentTime += min;
             flag--;
-
+        // If there is no process available to run at currentTime, current time is incremented until any process will be available to run
         } else {
             var timeStart = currentTime;
-
             do {
                 currentTime++;
                 var temp = processesCopy.filter(p => p.arrivingTime <= currentTime && (!finalExecution.includes(p) || p.name === 'idle'));
             } while (temp.length === 0);
-
+            // Adding idle status into final Execution
             finalExecution.push(new process("idle", timeStart, currentTime - timeStart, null, null, timeStart, currentTime, 0, 0));
-
         }
     } while (flag != 0);
 
     var averageWait = averageWait / finalExecution.length;
     return [finalExecution, averageWait];
 };
+
+
+// First Come First Serve function
 
 export const FCFS = (processesCopy) => {
 
@@ -58,6 +65,7 @@ export const FCFS = (processesCopy) => {
     var finalExecution = [];
     var averageWait = 0;
 
+    // flag is used to check whether all processes in processesCopy is finished or not
     var flag = processesCopy.length;
 
     for (let i = 0; i < processesCopy.length; i++) {
@@ -71,8 +79,10 @@ export const FCFS = (processesCopy) => {
     }
 
     do {
+        // filtered stores processes that are available to run at current time
         var filtered = processesCopy.filter(p => p.arrivingTime <= currentTime && (!finalExecution.includes(p) || p.name === 'idle'));
         if (filtered.length != 0) {
+            // min stores the arriving time of process that has minimum arriving time among other processes that are available to run
             var min = filtered[0].arrivingTime;
             var index = 0;
             for (let i = 0; i < filtered.length; i++) {
@@ -95,15 +105,14 @@ export const FCFS = (processesCopy) => {
             currentTime += filtered[index].cpuBurstTime1;
             flag--;
 
+        // If there is no process available to run at currentTime, current time is incremented until any process will be available to run
         } else {
             var timeStart = currentTime;
-
             do {
-
                 currentTime++;
                 var temp = processesCopy.filter(p => p.arrivingTime <= currentTime && (!finalExecution.includes(p) || p.name === 'idle'));
             } while (temp.length === 0);
-
+            // Adding idle status into final Execution
             finalExecution.push(new process("idle", timeStart, currentTime - timeStart, null, null, timeStart, currentTime, 0, 0));
 
         }
@@ -112,6 +121,9 @@ export const FCFS = (processesCopy) => {
     var averageWait = averageWait / finalExecution.length;
     return [finalExecution, averageWait];
 };
+
+
+// Shortest Remaining Time function
 
 export const SRTF = (processesCopy) => {
 
@@ -148,9 +160,13 @@ export const SRTF = (processesCopy) => {
     }
 
     var currentTime = 0;
+
+    // flag is used to check whether all processes in processesCopy is finished or not
     var flag = processesCopy.length;
 
     do {
+
+        // Sort processes that are available to run at current time by their CPU burst times
 
         for (let i = 0; i < processesCopy.length; i++) {
             for (let j = 0; j < processesCopy.length - i - 1; j++) {
@@ -162,27 +178,36 @@ export const SRTF = (processesCopy) => {
             }
         }
 
+        // Select the first process
+
         var p = processesCopy[0];
         if (currentTime >= p.arrivingTime) {
+            // timeStart is used to store the time process starts
             var timeStart = currentTime;
+            // cpuStart is used to store the inital CPU burst time when process starts
             var cpuStart = p.cpuBurstTime1;
+            // flag2 is used to check is there any processes running
             var flag2 = true;
             do {
 
+                // Process is running until it is finished or new process(or processes) come (or available to run at current time)
                 do {
                     currentTime++;
                     p.cpuBurstTime1--;
                 } while (p.cpuBurstTime1 != 0 && !arrivings.includes(currentTime));
 
-
+                // If process is finished, it is added to final execution and remove from the processesCopy
                 if (p.cpuBurstTime1 === 0) {
                     finalExecution.push(new process(p.name, timeStart, currentTime - timeStart, null, null, timeStart, currentTime, timeStart - p.arrivingTime, currentTime - p.arrivingTime));
                     flag--;
                     processesCopy = processesCopy.filter(t => t != p);
                     flag2 = false;
                 }
+                // If new process or processes come
                 else if (arrivings.includes(currentTime)) {
+                    // Get new process or processes
                     var temp = processesCopy.filter(t1 => t1.arrivingTime === currentTime);
+                    // Sort new processes
                     for (let i = 0; i < temp.length; i++) {
                         for (let j = 0; j < temp.length - i - 1; j++) {
                             if (temp[j].cpuBurstTime1 > temp[j + 1].cpuBurstTime1) {
@@ -192,6 +217,8 @@ export const SRTF = (processesCopy) => {
                             }
                         }
                     }
+                    // If new process's CPU burst time > current process's remaining CPU burst time,
+                    // current process will go to end of processCopy and new process will be added to beginning of processCopy
                     if (temp[0].cpuBurstTime1 < p.cpuBurstTime1) {
                         finalExecution.push(new process(p.name, timeStart, cpuStart - p.cpuBurstTime1, null, null, timeStart, currentTime, timeStart - p.arrivingTime, currentTime - p.arrivingTime));
                         p.arrivingTime = currentTime;
@@ -202,12 +229,13 @@ export const SRTF = (processesCopy) => {
                     }
                 }
             } while (flag2 === true)
-
+        // If there is no process available to run at currentTime, current time is incremented until any process will be available to run
         } else {
             var timeStart = currentTime;
             do {
                 currentTime++;
             } while (currentTime != p.arrivingTime);
+            // Adding idle status into final Execution
             finalExecution.push(new process('idle', timeStart, currentTime - timeStart, null, null, timeStart, currentTime, 0, 0));
         }
 
@@ -240,11 +268,13 @@ export const SRTF = (processesCopy) => {
 
 
 
+// Round Robin function
+
 export const RR = (processesCopy, quantum) => {
 
     var finalExecution = [];
 
-    // Sorting by arriving time
+    // Sorting by arriving time and getting the process names
 
     var processNames = [];
 
@@ -258,8 +288,9 @@ export const RR = (processesCopy, quantum) => {
             }
         }
     }
-
     var currentTime = 0;
+
+    // flag is used to check whether all processes in processesCopy is finished or not
     var flag = processesCopy.length;
 
     do {
@@ -273,20 +304,21 @@ export const RR = (processesCopy, quantum) => {
             var flag2 = true;
             do {
 
+                // Process is running until it is finish or quantum time is done
                 do {
                     currentTime++;
                     p.cpuBurstTime1--;
                     quantumStart--;
                 } while (p.cpuBurstTime1 != 0 && quantumStart != 0);
 
-
+                // If process is finished it will be added into finalExecution and removed from processCopy
                 if (p.cpuBurstTime1 === 0) {
                     finalExecution.push(new process(p.name, timeStart, currentTime - timeStart, null, null, timeStart, currentTime, timeStart - p.arrivingTime, currentTime - p.arrivingTime));
                     flag--;
                     processesCopy = processesCopy.filter(t => t != p);
                     flag2 = false;
                 }
-
+                // If the execution stop because of the quantum time is done, it will go into the end of the processCopy
                 else if (quantumStart === 0) {
                     finalExecution.push(new process(p.name, timeStart, cpuStart - p.cpuBurstTime1, null, null, timeStart, currentTime, timeStart - p.arrivingTime, currentTime - p.arrivingTime));
                     p.arrivingTime = currentTime;
@@ -295,12 +327,14 @@ export const RR = (processesCopy, quantum) => {
                     flag2 = false;
                 }
             } while (flag2 === true)
-
+        
+        // If there is no process available to run at currentTime, current time is incremented until any process will be available to run
         } else {
             var timeStart = currentTime;
             do {
                 currentTime++;
             } while (currentTime != p.arrivingTime);
+            // Adding idle status into final Execution
             finalExecution.push(new process('idle', timeStart, currentTime - timeStart, null, null, timeStart, currentTime, 0, 0));
         }
 
