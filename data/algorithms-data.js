c9b2ceaa69252823f7d33f142a2d2a3fcea1dd2a
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-06-01 14:12:04 +0300

    PS & PSNP added

--- data/algorithms-data.js
+++ data/algorithms-data.js
@@ -5,4 +5,6 @@ export const ALGORITHMS = [
     new algorithm('1a','SRTF',"SRTF",true,"SRTF"),
     new algorithm('2a','FCFS',"FCFS",false,"FCFS"),
     new algorithm('3a','Round Robin',"RR",true,"RR"),
+    new algorithm('4a','Priority Scheduling (Preemptive)',"PSP",true,"PSP"),
+    new algorithm('5a','Priority Scheduling (Non Preemptive)',"PSNP",false,"PSNP"),
 ]
