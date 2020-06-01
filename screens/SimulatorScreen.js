cfbc3a369e029e44d9c58a4fc2c2f771f4bb6cbd
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-06-01 14:13:10 +0300

    changes for PSP&PSNP

--- screens/SimulatorScreen.js
+++ screens/SimulatorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Alert, KeyboardAvoidingView, ActivityIndicator , Switch , Picker } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Alert, KeyboardAvoidingView, ActivityIndicator , Switch , Picker, Platform } from 'react-native';
 import { ALGORITHMS } from '../data/algorithms-data';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import CustomButton from '../components/CustomButton';
@@ -17,23 +17,27 @@ const SimulatorScreen = props => {
     const selectedAlgorithm = ALGORITHMS.find(item => item.id === algorithmId);
 
     const InitialProcesses = [
-        new process('P0',0,0,0,0,0,0,0,0),
-        new process('P1',0,0,0,0,0,0,0,0),
-        new process('P2',0,0,0,0,0,0,0,0),
-        new process('P3',0,0,0,0,0,0,0,0),
+        new process('P0',0,0,0,0,0,0,0,0,0),
+        new process('P1',0,0,0,0,0,0,0,0,0),
+        new process('P2',0,0,0,0,0,0,0,0,0),
+        new process('P3',0,0,0,0,0,0,0,0,0),
     ];
 
     const [processesList, setProcessesList] = useState(InitialProcesses);
     const [chartEnable, setChartEnable] = useState(false);
     const [quantum, setQuantum] = useState(0);
-    const [quantumRef, setQuantumRef] = useState(0);
     const [quantumIO, setQuantumIO] = useState(0);
-    const [quantumIORef, setQuantumIORef] = useState(0);
+
     const [loading, setLoading] = useState(false);
-    const [IOdevice,setIOdevice] = useState(false)
-    const [selectedIO,setSelectedIO] = useState("FCFS")
+    const [IOdevice,setIOdevice] = useState(false);
+    const [selectedIO,setSelectedIO] = useState("FCFS");
+    const [selectedPriority,setSelectedPriority] = useState("Smallest")
 
     // Input References
+
+    const [quantumIORef, setQuantumIORef] = useState(0);
+    const [quantumRef, setQuantumRef] = useState(0);
+
     const [refInput00, setRefInput00] = useState(0);
     const [refInput01, setRefInput01] = useState(0);
     const [refInput10, setRefInput10] = useState(0);
@@ -55,6 +59,12 @@ const SimulatorScreen = props => {
     const [IOrefInput31, setIORefInput31] = useState(0);
     const [IOrefInput40, setIORefInput40] = useState(0);
     const [IOrefInput41, setIORefInput41] = useState(0);
+
+    const [PriorityRef0,setPriorityRef0] = useState(0);
+    const [PriorityRef1,setPriorityRef1] = useState(0);
+    const [PriorityRef2,setPriorityRef2] = useState(0);
+    const [PriorityRef3,setPriorityRef3] = useState(0);
+    const [PriorityRef4,setPriorityRef4] = useState(0);
 
     const renderProcesses = (itemData) => {
 
@@ -181,6 +191,37 @@ const SimulatorScreen = props => {
                         />
                     </View>:<></>
                     }
+
+                {selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP" ? 
+                    <View style={styles.itemContainer}>
+                        <TextInput
+                            returnKeyType = 'next'
+                            ref={refInputin => {
+                                if (itemData.item.name === 'P0') setPriorityRef0(refInputin);
+                                else if (itemData.item.name === 'P1') setPriorityRef1(refInputin);
+                                else if (itemData.item.name === 'P2') setPriorityRef2(refInputin);
+                                else if (itemData.item.name === 'P3') setPriorityRef3(refInputin);
+                                else if (itemData.item.name === 'P4') setPriorityRef4(refInputin);
+                            }}
+                            onSubmitEditing={() => {
+                                if (itemData.item.name === 'P0' && processesList.length > 1) PriorityRef1.focus();
+                                else if (itemData.item.name === 'P1' && processesList.length > 2) PriorityRef2.focus();
+                                else if (itemData.item.name === 'P2' && processesList.length > 3) PriorityRef3.focus();
+                                else if (itemData.item.name === 'P3' && processesList.length > 4) PriorityRef4.focus();
+                            }}
+                            editable={!chartEnable}
+                            placeholder='Edit'
+                            keyboardType='number-pad'
+                            onChangeText={input => {
+                                processesList[selectedProcessId].priority = parseInt(input);
+                                setProcessesList(processesList);
+                            }}
+                            value={processesList[selectedProcessId].priority}
+                            style={styles.input}
+                        />
+                    </View>:<></>
+                    }
+                
             </View>
         );
     };
@@ -207,7 +248,7 @@ const SimulatorScreen = props => {
                                 <View style={styles.IOContainer}>
                                     <Text style={styles.IOchoiceLabel}>I/O Algorithm: </Text>
                                     <Picker
-                                        enabled = {IOdevice}
+                                        enabled = {!chartEnable}
                                         selectedValue={selectedIO}
                                         style={styles.IOalgorithmPicker}
                                         onValueChange={ value => setSelectedIO(value)}
@@ -219,6 +260,21 @@ const SimulatorScreen = props => {
                                         <Picker.Item label="RR" value="RR" />
                                     </Picker>
                                 </View>
+                                {selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP" ? 
+                                    <View style={styles.IOContainer}>
+                                        <Text style={styles.IOchoiceLabel}>Priority: </Text>
+                                        <Picker
+                                            enabled = {!chartEnable}
+                                            selectedValue={selectedPriority}
+                                            style={styles.IOalgorithmPicker}
+                                            onValueChange={ value => setSelectedPriority(value)}
+                                            //mode="dropdown"
+                                        >
+                                            <Picker.Item label="Smallest" value="Smallest" />
+                                            <Picker.Item label="Largest" value="Largest" />
+                                        </Picker>
+                                    </View>
+                                :<></>}
                             </View>}
                         <View style={{ ...styles.rows, borderBottomWidth: 2, marginBottom: 5 }}>
                             <View style={styles.itemContainer}>
@@ -228,13 +284,13 @@ const SimulatorScreen = props => {
                             </View>
                             <View style={styles.itemContainer}>
                                 <Text style={styles.caption}>
-                                    {IOdevice ? "Arrive" : "Arriving Time" }
+                                    {IOdevice || selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP" ? "Arrive" : "Arriving Time" }
                                 </Text>
                             </View>
 
                             <View style={styles.itemContainer}>
                                 <Text style={styles.caption}>
-                                    {IOdevice ? "Cpu" : "Cpu Burst" }
+                                    {IOdevice || selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP" ? "Cpu" : "Cpu Burst" }
                                 </Text>
                             </View>
 
@@ -254,6 +310,14 @@ const SimulatorScreen = props => {
                             </View>
                             :<></>
                             }
+                            {selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP" ?
+                            <View style={styles.itemContainer}>
+                                <Text style={styles.caption}>
+                                    {IOdevice ? "Pri." : "Priority" }
+                                </Text>
+                            </View>
+                            :<></>
+                            }
 
                         </View>
                         <KeyboardAvoidingView behavior="padding">
@@ -262,8 +326,9 @@ const SimulatorScreen = props => {
                                 data={processesList}
                                 renderItem={renderProcesses}
                             />
-                            {selectedAlgorithm.shortName === "RR" ?
+                            
                                 <View style={styles.quantumMain}>
+                                {selectedAlgorithm.shortName === "RR" ?
                                     <View style={styles.quantumContainer}>
                                         <View style={styles.quantumLabel}>
                                             <Text>Quantum : </Text>
@@ -280,7 +345,7 @@ const SimulatorScreen = props => {
                                                 value={quantum}
                                             />
                                         </View>
-                                    </View>
+                                    </View> : <></>}
                                     {selectedIO==="RR" ? 
                                         <View style={styles.quantumContainer}>
                                             <View style={styles.quantumLabel}>
@@ -300,9 +365,6 @@ const SimulatorScreen = props => {
                                             </View>
                                         </View>:<></>}
                                 </View>
-                                : <></>
-
-                            }
                         </KeyboardAvoidingView>
 
                         {loading ?
@@ -319,6 +381,7 @@ const SimulatorScreen = props => {
                                 selectedIO = {selectedIO}
                                 IOdevice = {IOdevice}
                                 quantumIO = {quantumIO}
+                                selectedPriority = {selectedPriority}
                             /> : <></>}
 
                     </View>
@@ -362,12 +425,18 @@ const SimulatorScreen = props => {
                                         IOrefInput00.clear();
                                         IOrefInput01.clear();                                        
                                     }
+                                    if(selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP"){
+                                        PriorityRef0.clear();
+                                    }
                                     if (--L === 0) break;
                                     refInput10.clear();
                                     refInput11.clear();
                                     if(IOdevice){
                                         IOrefInput10.clear();
                                         IOrefInput11.clear();                                        
+                                    }
+                                    if(selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP"){
+                                        PriorityRef1.clear();
                                     }
                                     if (--L === 0) break;
                                     refInput20.clear();
@@ -376,6 +445,9 @@ const SimulatorScreen = props => {
                                         IOrefInput20.clear();
                                         IOrefInput21.clear();                                        
                                     }
+                                    if(selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP"){
+                                        PriorityRef2.clear();
+                                    }
                                     if (--L === 0) break;
                                     refInput30.clear();
                                     refInput31.clear();
@@ -383,12 +455,18 @@ const SimulatorScreen = props => {
                                         IOrefInput30.clear();
                                         IOrefInput31.clear();                                        
                                     }
+                                    if(selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP"){
+                                        PriorityRef3.clear();
+                                    }
                                     if (--L === 0) break;
                                     refInput40.clear();
                                     refInput41.clear();
                                     if(IOdevice){
                                         IOrefInput40.clear();
                                         IOrefInput41.clear();                                        
+                                    }
+                                    if(selectedAlgorithm.shortName === "PSP"  || selectedAlgorithm.shortName === "PSNP"){
+                                        PriorityRef4.clear();
                                     }
                                 }
                                 if (selectedAlgorithm.shortName === 'RR') {
@@ -414,7 +492,13 @@ const SimulatorScreen = props => {
                                         return (null);
                                     } else {
                                         for (let i = 0; i < processesList.length; i++) {
-                                            if (isNaN(processesList[i].arrivingTime) || isNaN(processesList[i].cpuBurstTime1) || isNaN(processesList[i].IOBurstTime) || isNaN(processesList[i].cpuBurstTime2) ||  processesList[i].cpuBurstTime1 === 0) {
+                                            if (
+                                                (isNaN(processesList[i].arrivingTime) || processesList[i].arrivingTime<0)  || 
+                                                (isNaN(processesList[i].cpuBurstTime1) || processesList[i].cpuBurstTime1<0) || 
+                                                (isNaN(processesList[i].IOBurstTime) || processesList[i].IOBurstTime<0) || 
+                                                (isNaN(processesList[i].cpuBurstTime2) || processesList[i].cpuBurstTime2<0)||
+                                                (isNaN(processesList[i].priority) || processesList[i].priority<0)||
+                                                processesList[i].cpuBurstTime1 === 0) {
                                                 Alert.alert("Invalid values!", "Please check the values !", [{ text: 'OK' }]);
                                                 inputValidation = false;
                                                 break;
@@ -511,10 +595,12 @@ const styles = StyleSheet.create({
     input: {
         flex: 1,
         width: '100%',
-        textAlign: 'center',
+        textAlign: Platform.OS === 'android' && Platform.Version >= 26 ? 'center' : 'left',
+        marginLeft : Platform.OS === 'android' && Platform.Version >= 26 ? 0 : 45,
     },
     IOchoiceContainer : {
         flexDirection: 'row',
+        flex: 1,
     },
     IOchoiceLabel :{
         fontFamily : 'open-sans',
@@ -526,7 +612,7 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     IOContainer: {
-        width: '50%',
+        flex:1,
         paddingLeft:10,
         paddingRight:10
     },
@@ -540,8 +626,15 @@ SimulatorScreen.navigationOptions = navigationData => {
     const algorithmId = navigationData.navigation.getParam('algorithmId');
     const selectedAlgorithm = ALGORITHMS.find(item => item.id === algorithmId);
 
+    var title = selectedAlgorithm.name;
+    if(selectedAlgorithm.shortName === "PSP"){
+        title = "Priority Scheduling (P)"
+    }else if(selectedAlgorithm.shortName === "PSNP"){
+        title = "Priority Scheduling (NP)"
+    }
+
     return ({
-        headerTitle: selectedAlgorithm.name,
+        headerTitle: title,
     })
 }
 
