239b53b11c5ff362ca59a5681c60c3ca98cd3b6b
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-06-06 11:57:57 +0300

    warnings

--- App.js
+++ App.js
@@ -31,6 +31,7 @@ export default function App() {
 
   return (
       <>
+        {console.disableYellowBox = true}
         <StatusBar backgroundColor={Colors.red}  />
         <AppNavigator />
       </>
